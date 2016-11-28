c283aa7cf6e97767e584f4faaf736e1457ac034c
Moi Gtz <moyoteg@gmail.com>
2016-11-28 16:28:09 -0700

    deployed to heroku and played a little with it

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,9 +6,19 @@ drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
+	
+	// http://0.0.0.0:8080
 }
 
 
+
+drop.get ("beers", Int.self) { request, beers in
+	
+	return try JSON(node: ["message": "Take one down, pass it around, \(beers - 1 ) bottles of beer on the wall..."])
+
+	// http://0.0.0.0:8080/beers/8
+	
+}
 
 drop.resource("posts", PostController())
 
